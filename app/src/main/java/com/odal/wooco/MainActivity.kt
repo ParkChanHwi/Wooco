93e81c69a785a2fcde9f47606ee7c3c8ee573ea7
dlagu <imteddy0311@gmail.com>
2024-05-16 16:42:00 +0900

    hj modify, add files rectangle ~

--- app/src/main/java/com/odal/wooco/MainActivity.kt
+++ app/src/main/java/com/odal/wooco/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_register)
+        setContentView(R.layout.activity_phcheck)
 
     }
 }
