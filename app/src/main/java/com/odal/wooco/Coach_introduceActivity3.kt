b960ec91e79e8279a482ec5f7af6ad837293c1ba
yuna <2002yuna@naver.com>
2024-05-29 21:32:40 +0900

    schedule, coach_introduce1,2,3

---
+++ app/src/main/java/com/odal/wooco/Coach_introduceActivity3.kt
@@ -0,0 +1,20 @@
+package com.odal.wooco
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Coach_introduceActivity3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_coach_introduce3)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
