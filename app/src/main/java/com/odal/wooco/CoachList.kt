0be9c7417d1a76db83a3ac87b33c25d145f07024
dlagu <imteddy0311@gmail.com>
2024-05-29 21:37:13 +0900

    bottomsheet 수정 (연동 확인)

--- app/src/main/java/com/odal/wooco/CoachList.kt
+++ app/src/main/java/com/odal/wooco/CoachList.kt
@@ -1,20 +1,21 @@
 package com.odal.wooco
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+
+
+
+
 
 class CoachList : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.menti_coachlist)
-
         findViewById<Button>(R.id.kategori1).setOnClickListener {
-            Log.d("MainActivity", "TextView clicked")
-            val bottomSheet = MyBottomSheetDialogFragment()
-            bottomSheet.show(supportFragmentManager, "MyBottomSheetDialogFragment")
+            val bottomSheet = BottomSheet()
+            bottomSheet.show(supportFragmentManager, bottomSheet.tag)
         }
     }
 }
-// 아래 bottom sheet 띄우려다가 실패한 코드
+
