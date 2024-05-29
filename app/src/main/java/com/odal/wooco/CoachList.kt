913536c5f6a269b78f9409fd250537b994cd3c1d
Chajongho <djev33@gmail.com>
2024-05-29 23:39:20 +0900

    바텀시트 띄우기, 메인화면 코치리스트로 바꾸기 성공~

--- app/src/main/java/com/odal/wooco/CoachList.kt
+++ app/src/main/java/com/odal/wooco/CoachList.kt
@@ -3,11 +3,13 @@ package com.odal.wooco
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 class CoachList : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.menti_coachlist)
 
         findViewById<TextView>(R.id.kategori1).setOnClickListener {
