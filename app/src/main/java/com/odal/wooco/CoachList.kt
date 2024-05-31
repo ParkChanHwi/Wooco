cea0d928b7bb82d361f0c226a405a29d1e19cbef
ParkChanHwi <56109340+ParkChanHwi@users.noreply.github.com>
2024-05-31 17:35:50 +0900

    Chan update

--- app/src/main/java/com/odal/wooco/CoachList.kt
+++ app/src/main/java/com/odal/wooco/CoachList.kt
@@ -1,7 +1,13 @@
 package com.odal.wooco
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
@@ -11,9 +17,43 @@ class CoachList : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.menti_coachlist)
 
+        val homeBtn:ImageView = findViewById(R.id.material_sy)
+        val chatBtn:ImageView = findViewById(R.id.chat_1)
+        val calBtn:ImageView = findViewById(R.id.uiw_date)
+        val profileBtn:ImageView = findViewById(R.id.group_513866)
+
+
         findViewById<Button>(R.id.kategori1).setOnClickListener {
             val bottomSheet = BottomSheet()
             bottomSheet.show(supportFragmentManager, bottomSheet.tag)
         }
+
+
+
+        homeBtn.setOnClickListener{
+            Toast.makeText(this, "현재 화면입니다.", Toast.LENGTH_SHORT).show()
+//            val intent = Intent(this, CoachList::class.java)
+//            startActivity(intent)
+        }
+
+        // 아직 채팅방 형성 x
+//        chatBtn.setOnClickListener{
+//            val intent = Intent(this, coachChat::class.java)
+//            startActivity(intent)
+//        }
+
+        calBtn.setOnClickListener{
+            val intent = Intent(this, Coach_scheduleActivity::class.java)
+            startActivity(intent)
+        }
+
+        // 아직 마이페이지 형성 x
+//        profileBtn.setOnClickListener{
+//            val intent = Intent(this, Coach_profileActivity::class.java)
+//            startActivity(intent)
+//        }
+
+
+
     }
 }
--- app/src/main/java/com/odal/wooco/Menti_scheduleActivity.kt
+++ app/src/main/java/com/odal/wooco/Menti_scheduleActivity.kt
@@ -1,6 +1,9 @@
 package com.odal.wooco
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +14,42 @@ class Menti_scheduleActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.menti_schedule)
+
+        val homeBtn: ImageView = findViewById(R.id.material_sy)
+        val chatBtn: ImageView = findViewById(R.id.chat_1)
+        val calBtn: ImageView = findViewById(R.id.uiw_date)
+        val profileBtn: ImageView = findViewById(R.id.group_513866)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        homeBtn.setOnClickListener{
+            val intent = Intent(this, CoachList::class.java)
+           startActivity(intent)
+        }
+
+        // 아직 채팅방 형성 x
+//        chatBtn.setOnClickListener{
+//            val intent = Intent(this, coachChat::class.java)
+//            startActivity(intent)
+//        }
+
+        calBtn.setOnClickListener{
+            Toast.makeText(this, "현재 화면입니다.", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, Coach_scheduleActivity::class.java)
+            startActivity(intent)
+        }
+
+        // 아직 마이페이지 형성 x
+//        profileBtn.setOnClickListener{
+//            val intent = Intent(this, Coach_profileActivity::class.java)
+//            startActivity(intent)
+//        }
+
+
     }
 }
