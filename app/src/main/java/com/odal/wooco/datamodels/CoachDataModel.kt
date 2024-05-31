69b3290085c3fe960cc8589ff4bf04947c9dff6f
ParkChanHwi <56109340+ParkChanHwi@users.noreply.github.com>
2024-05-31 22:26:12 +0900

    ch yn commit

---
+++ app/src/main/java/com/odal/wooco/datamodels/CoachDataModel.kt
@@ -0,0 +1,10 @@
+package com.odal.wooco.datamodels
+
+class CoachDataModel (
+    //Coach_Adapter.Item("차우코", "강원대 00학과", "자격증 - 기사/기능사, 진로", "4.9"),
+    val uid : String? = null,
+    val nickname : String? = null,
+    val school : String? = null,
+    val interest : String? = null,
+    val score : Double? = null
+)
