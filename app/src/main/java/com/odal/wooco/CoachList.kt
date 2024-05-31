f466ecea1e652c68e7d5dce5b34eb6aa2ba54d97
Chajongho <djev33@gmail.com>
2024-05-31 21:26:27 +0900

    coach_myself.xml -> edittext로 수정 , menti_univ_fragment_bottom_sheet.xml~5 완성, 카테고리중 편입삭제

--- app/src/main/java/com/odal/wooco/CoachList.kt
+++ app/src/main/java/com/odal/wooco/CoachList.kt
@@ -11,47 +11,49 @@ class CoachList : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.menti_coachlist)
+        setContentView(R.layout.coach_myself)
 
-        findViewById<Button>(R.id.kategori1).setOnClickListener {
-            val bottomSheet = BottomSheet1()
-            bottomSheet.show(supportFragmentManager, bottomSheet.tag)
-        }
-        findViewById<Button>(R.id.kategori2).setOnClickListener {
-            val bottomSheet = BottomSheet2()
-            bottomSheet.show(supportFragmentManager, bottomSheet.tag)
-        }
-        findViewById<Button>(R.id.kategori3).setOnClickListener {
-            val bottomSheet = BottomSheet3()
-            bottomSheet.show(supportFragmentManager, bottomSheet.tag)
-        }
-        findViewById<Button>(R.id.kategori4).setOnClickListener {
-            val bottomSheet = BottomSheet4()
-            bottomSheet.show(supportFragmentManager, bottomSheet.tag)
-        }
-        findViewById<Button>(R.id.kategori5).setOnClickListener {
-            val bottomSheet = BottomSheet5()
-            bottomSheet.show(supportFragmentManager, bottomSheet.tag)
-        }
-        findViewById<Button>(R.id.kategori6).setOnClickListener {
-            val bottomSheet = BottomSheet6()
-            bottomSheet.show(supportFragmentManager, bottomSheet.tag)
-        }
-        findViewById<Button>(R.id.kategori7).setOnClickListener {
-            val bottomSheet = BottomSheet7()
-            bottomSheet.show(supportFragmentManager, bottomSheet.tag)
-        }
-
-        val recyclerView: RecyclerView = findViewById(R.id.coachlist_recycleView)
-
-        val items = listOf(
-            Coach_Adapter.Item("차우코", "강원대 00학과", "자격증 - 기사/기능사, 진로", "4.9"),
-            Coach_Adapter.Item("별명 2", "학교/회사 2", "관심분야 2", "4.5"),
-            // Add more items as needed
-        )
-
-        val adapter = Coach_Adapter(items)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = adapter
+//        findViewById<Button>(R.id.kategori1).setOnClickListener {
+//            val bottomSheet = BottomSheet1()
+//            bottomSheet.show(supportFragmentManager, bottomSheet.tag)
+//        }
+//        findViewById<Button>(R.id.kategori2).setOnClickListener {
+//            val bottomSheet = BottomSheet2()
+//            bottomSheet.show(supportFragmentManager, bottomSheet.tag)
+//        }
+//        findViewById<Button>(R.id.kategori3).setOnClickListener {
+//            val bottomSheet = BottomSheet3()
+//            bottomSheet.show(supportFragmentManager, bottomSheet.tag)
+//        }
+//        findViewById<Button>(R.id.kategori4).setOnClickListener {
+//            val bottomSheet = BottomSheet4()
+//            bottomSheet.show(supportFragmentManager, bottomSheet.tag)
+//        }
+//        findViewById<Button>(R.id.kategori5).setOnClickListener {
+//            val bottomSheet = BottomSheet5()
+//            bottomSheet.show(supportFragmentManager, bottomSheet.tag)
+//        }
+//        findViewById<Button>(R.id.kategori6).setOnClickListener {
+//            val bottomSheet = BottomSheet6()
+//            bottomSheet.show(supportFragmentManager, bottomSheet.tag)
+//        }
+//
+//        val recyclerView: RecyclerView = findViewById(R.id.coachlist_recycleView)
+//
+//        val items = listOf(
+//            Coach_Adapter.Item("차우코", "강원대 00학과", "자격증 - 기사/기능사, 진로", "4.9"),
+//            Coach_Adapter.Item("별명 2", "학교/회사 2", "관심분야 2", "4.5"),
+//            Coach_Adapter.Item("별명 2", "학교/회사 2", "관심분야 2", "4.5"),
+//            Coach_Adapter.Item("별명 2", "학교/회사 2", "관심분야 2", "4.5"),
+//            Coach_Adapter.Item("별명 2", "학교/회사 2", "관심분야 2", "4.5"),
+//            Coach_Adapter.Item("별명 2", "학교/회사 2", "관심분야 2", "4.5"),
+//            Coach_Adapter.Item("별명 2", "학교/회사 2", "관심분야 2", "4.5"),
+//            Coach_Adapter.Item("별명 2", "학교/회사 2", "관심분야 2", "4.5"),
+//            // Add more items as needed
+//        )
+//
+//        val adapter = Coach_Adapter(items)
+//        recyclerView.layoutManager = LinearLayoutManager(this)
+//        recyclerView.adapter = adapter
     }
 }
--- app/src/main/java/com/odal/wooco/MyBottomSheetDialogFragment.kt
+++ app/src/main/java/com/odal/wooco/MyBottomSheetDialogFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 
@@ -60,21 +59,10 @@ class BottomSheet5() : BottomSheetDialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         super.onCreateView(inflater, container, savedInstanceState)
-        return inflater.inflate(R.layout.menti_univ_fragment_bottom_sheet3, container, false)
+        return inflater.inflate(R.layout.menti_univ_fragment_bottom_sheet5, container, false)
     }
 }
 class BottomSheet6() : BottomSheetDialogFragment() {
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        super.onCreateView(inflater, container, savedInstanceState)
-        return inflater.inflate(R.layout.menti_univ_fragment_bottom_sheet4, container, false)
-    }
-}
-class BottomSheet7() : BottomSheetDialogFragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
