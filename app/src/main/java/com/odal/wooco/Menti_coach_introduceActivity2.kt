4d8bc82b196c46fa7fe493258e0a2fcff354f15b
yuna <2002yuna@naver.com>
2024-06-02 13:30:50 +0900

    editt

--- app/src/main/java/com/odal/wooco/Menti_coach_introduceActivity2.kt
+++ app/src/main/java/com/odal/wooco/Menti_coach_introduceActivity2.kt
@@ -1,20 +1,28 @@
 package com.odal.wooco
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class Menti_coach_introduceActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.menti_coach_introduce2)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val recyclerView: RecyclerView = findViewById(R.id.menti_coach_introduce_recycler_view)
+
+        val items = listOf(
+            Menti_coach_introduceActivity2Adapter.Item("한 줄 소개", "수업 가능 과목", "차별점","수업 방식")
+
+            // Add more items as needed
+
+        )
+        val adapter = Menti_coach_introduceActivity2Adapter(items)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
     }
 }
