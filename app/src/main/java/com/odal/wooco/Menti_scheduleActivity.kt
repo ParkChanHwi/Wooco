2a5874c95a64eed9e60efee8c9cb89bfb440cf05
ParkChanHwi <56109340+ParkChanHwi@users.noreply.github.com>
2024-06-03 23:46:49 +0900

    Menti_scheduleActivity updated menti information

--- app/src/main/java/com/odal/wooco/Menti_scheduleActivity.kt
+++ app/src/main/java/com/odal/wooco/Menti_scheduleActivity.kt
@@ -2,20 +2,31 @@ package com.odal.wooco
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.odal.wooco.utils.FirebaseRef.Companion.userInfoRef
 
 class Menti_scheduleActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.menti_schedule)
 
+        // auth initialize
+        auth = FirebaseAuth.getInstance()
+        val currentUser = auth.currentUser
         val homeBtn: ImageView = findViewById(R.id.material_sy)
         val chatBtn: ImageView = findViewById(R.id.chat_1)
         val calBtn: ImageView = findViewById(R.id.uiw_date)
@@ -37,6 +48,24 @@ class Menti_scheduleActivity : AppCompatActivity() {
         val adapter = Menti_sheduleActivityAdapter(items)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = adapter
+
+
+        userInfoRef.child(currentUser?.uid ?: "").child("nickname").addListenerForSingleValueEvent(object :
+            ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val name = snapshot.getValue(String::class.java)
+
+                // 가져온 이름을 화면에 표시
+                val scheduleNameTextView: TextView = findViewById(R.id.user_class_text)
+                scheduleNameTextView.text = name
+
+                Log.d("UserDisplayName", "Current user display name: $name")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.e("Firebase", "Failed to read value.", error.toException())
+            }
+        })
 
 
         homeBtn.setOnClickListener{
