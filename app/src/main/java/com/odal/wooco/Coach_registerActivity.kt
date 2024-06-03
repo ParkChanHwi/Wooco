5d3c862d94382ca8d133b1a51e2b71d772666ed0
yuna <2002yuna@naver.com>
2024-06-03 15:16:40 +0900

    introduce_edit

--- app/src/main/java/com/odal/wooco/Coach_registerActivity.kt
+++ app/src/main/java/com/odal/wooco/Coach_registerActivity.kt
@@ -1,20 +1,41 @@
 package com.odal.wooco
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-class Coach_registerActivity : AppCompatActivity() {
+class Coach_registerActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.coach_register)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val recyclerView: RecyclerView = findViewById(R.id.coach_register1_view)
+        val recyclerView2: RecyclerView = findViewById(R.id.coach_register2_view)
+
+
+        val items = listOf(
+            Coach_registerActivityAdapter.Item("카테고리", "세부카테고리"),
+            Coach_registerActivityAdapter.Item("카테고리", "세부카테고리"),
+            Coach_registerActivityAdapter.Item("카테고리", "세부카테고리"),
+            Coach_registerActivityAdapter.Item("카테고리", "세부카테고리"),
+            Coach_registerActivityAdapter.Item("카테고리", "세부카테고리"),
+            Coach_registerActivityAdapter.Item("카테고리", "세부카테고리"),
+        )
+        val adapter = Coach_registerActivityAdapter(items)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+
+        val items2 = listOf(
+            Coach_registerActivityAdapter2.Item(R.drawable.document, R.drawable.document, R.drawable.document),
+            Coach_registerActivityAdapter2.Item(R.drawable.document, R.drawable.document, R.drawable.document),
+            Coach_registerActivityAdapter2.Item(R.drawable.document, R.drawable.document, R.drawable.document)
+        )
+        val adapter2 = Coach_registerActivityAdapter2(items2)
+        recyclerView2.layoutManager = LinearLayoutManager(this)
+        recyclerView2.adapter = adapter2
     }
 }
---
+++ app/src/main/java/com/odal/wooco/Coach_registerActivityAdapter.kt
@@ -0,0 +1,33 @@
+package com.odal.wooco
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class Coach_registerActivityAdapter(private val itemList: List<Item>) : RecyclerView.Adapter<Coach_registerActivityAdapter.ItemViewHolder>() {
+
+    data class Item(
+        val category3: String,
+        val category4: String
+    )
+
+    class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val category3TextView: TextView = itemView.findViewById(R.id.category1)
+        val category4TextView: TextView = itemView.findViewById(R.id.category2)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.coach_register_item, parent, false)
+        return ItemViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = itemList[position]
+        holder.category3TextView.text = item.category3
+        holder.category4TextView.text = item.category4
+    }
+
+    override fun getItemCount(): Int = itemList.size
+}
