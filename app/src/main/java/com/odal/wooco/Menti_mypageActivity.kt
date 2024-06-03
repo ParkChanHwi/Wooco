9d2008ffc4c4074cc0598f0bba6673789a1a91e6
ParkChanHwi <56109340+ParkChanHwi@users.noreply.github.com>
2024-06-03 23:35:48 +0900

    menti_mypage updated menti information

--- app/src/main/java/com/odal/wooco/Menti_mypageActivity.kt
+++ app/src/main/java/com/odal/wooco/Menti_mypageActivity.kt
@@ -2,26 +2,63 @@ package com.odal.wooco
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class Menti_mypageActivity : AppCompatActivity() {
+    // declare auth
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.menti_mypage)
+        // auth initialize
+        auth = FirebaseAuth.getInstance()
         val homeBtn: ImageView = findViewById(R.id.material_sy)
         val chatBtn: ImageView = findViewById(R.id.chat_1)
         val calBtn: ImageView = findViewById(R.id.uiw_date)
         val profileBtn: ImageView = findViewById(R.id.group_513866)
         val transferBtn: Button = findViewById(R.id.coach_transfer)
 
-            homeBtn.setOnClickListener{
+
+        // Firebase Realtime Database에서 UserInfo 이름 가져오기
+        val database = FirebaseDatabase.getInstance()
+        val userInfoRef = database.getReference("userInfo")
+
+        // 현재 로그인한 사용자의 이름을 가져오기
+        val currentUser = auth.currentUser
+
+        userInfoRef.child(currentUser?.uid ?: "").child("nickname").addListenerForSingleValueEvent(object :
+            ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val name = snapshot.getValue(String::class.java)
+
+                // 가져온 이름을 화면에 표시
+                val myPageNameTextView: TextView = findViewById(R.id.mypage_name)
+                myPageNameTextView.text = name
+
+                Log.d("UserDisplayName", "Current user display name: $name")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.e("Firebase", "Failed to read value.", error.toException())
+            }
+        })
+
+
+        homeBtn.setOnClickListener{
                 val intent = Intent(this, CoachList::class.java)
                 startActivity(intent)
             }
