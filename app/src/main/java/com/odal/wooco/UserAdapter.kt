129a5d3f2b2682fceb8b4ce2f53d66dc151a55f4
yuna <2002yuna@naver.com>
2024-06-03 16:01:37 +0900

    merge_commit

--- app/src/main/java/com/odal/wooco/UserAdapter.kt
+++ app/src/main/java/com/odal/wooco/UserAdapter.kt
@@ -6,7 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.google.firebase.firestore.auth.User
+//import com.google.firebase.firestore.auth.User
 import com.odal.wooco.datamodels.UserDataModel
 
 //채팅 어뎁터
