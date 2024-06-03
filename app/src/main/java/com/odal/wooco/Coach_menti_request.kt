f0b28bec07be41e7234f94a12ce934e2cfc5c8ae
Chajongho <djev33@gmail.com>
2024-06-03 14:11:28 +0900

    예약 첫번째화면 + 멘티요청목록(미완)

---
+++ app/src/main/java/com/odal/wooco/Coach_menti_request.kt
@@ -0,0 +1,20 @@
+package com.odal.wooco
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Coach_menti_request : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.coach_menti_request)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
