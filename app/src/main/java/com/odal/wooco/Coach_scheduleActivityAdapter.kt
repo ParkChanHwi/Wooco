f2d9c3d383ae730b89040ba5ee9ec9a4bdadaf42
dlagu <imteddy0311@gmail.com>
2024-06-06 23:19:40 +0900

    6/6 코치 스케줄 보이게 하기, 시간 순대로 정렬하기, 코치&멘티 스케줄 아이템 xml 수정

--- app/src/main/java/com/odal/wooco/Coach_scheduleActivityAdapter.kt
+++ app/src/main/java/com/odal/wooco/Coach_scheduleActivityAdapter.kt
@@ -1,12 +1,14 @@
 package com.odal.wooco
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.odal.wooco.datamodels.ReserveDataModel
 
-class CoachScheduleActivityAdapter(private val itemList: List<Item>) : RecyclerView.Adapter<CoachScheduleActivityAdapter.ItemViewHolder>() {
+class CoachScheduleActivityAdapter(private var itemList: List<ReserveDataModel>, val context: Context) : RecyclerView.Adapter<CoachScheduleActivityAdapter.ItemViewHolder>() {
 
     data class Item(
         val name: String,
@@ -26,10 +28,15 @@ class CoachScheduleActivityAdapter(private val itemList: List<Item>) : RecyclerV
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = itemList[position]
-        holder.nameTextView.text = "${item.name}"
-        holder.dateTextView.text = "${item.date}"
+        holder.nameTextView.text = "${item.menti_name}"
+        holder.dateTextView.text = "${item.reserve_time}"
     }
 
-
     override fun getItemCount(): Int = itemList.size
+
+    // 수업 시작 시간에 따라 itemList를 정렬하는 메서드
+    fun sortByStartTime() {
+        itemList = itemList.sortedBy { it.reserve_time }
+        notifyDataSetChanged()
+    }
 }
