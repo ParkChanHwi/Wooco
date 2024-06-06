f21a3c0e3a539b7209cf45f9b780f68704ac64d1
ParkChanHwi <56109340+ParkChanHwi@users.noreply.github.com>
2024-06-06 16:50:56 +0900

    radio button click event

--- app/src/main/java/com/odal/wooco/utils/FirebaseRef.kt
+++ app/src/main/java/com/odal/wooco/utils/FirebaseRef.kt
@@ -11,5 +11,6 @@ class FirebaseRef {
         val userInfoRef = database.getReference("userInfo")
         val coachInfoRef = database.getReference("coachInfo")
         val chats = database.getReference("chats")
+        val reserveInfoRef = database.getReference("reserveInfo") //예약정보 데이터베이스 6/4(금)
     }
 }
