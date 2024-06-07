e5f6ce335c036e770a7869ceb3a8dc6a12bc58b4
dlagu <imteddy0311@gmail.com>
2024-06-07 19:46:25 +0900

    6/7 수업변경 해냄 !

--- app/src/main/java/com/odal/wooco/MentiReserve.kt
+++ app/src/main/java/com/odal/wooco/MentiReserve.kt
@@ -12,6 +12,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
@@ -23,33 +24,28 @@ class MentiReserve : AppCompatActivity() {
     private lateinit var coach_receiverName: String
     private lateinit var mentiName: String // 멘티의 이름 변수 추가
     private lateinit var auth: FirebaseAuth
+    private lateinit var reserveInfoRef: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.menti_reserve)
 
-        // auth initialize
         auth = FirebaseAuth.getInstance()
-
-        // Firebase Realtime Database에서 UserInfo 이름 가져오기
         val database = FirebaseDatabase.getInstance()
-        val userInfoRef = database.getReference("userInfo")
+        reserveInfoRef = database.getReference("reserveInfo")
 
-        // 현재 로그인한 사용자의 정보를 가져오기
         val currentUser = auth.currentUser
-        if (currentUser != null) {
-            val userId = currentUser.uid
+        currentUser?.let {
+            val userId = it.uid
+            val userInfoRef = database.getReference("userInfo")
             userInfoRef.child(userId).addListenerForSingleValueEvent(object : ValueEventListener {
                 override fun onDataChange(dataSnapshot: DataSnapshot) {
                     if (dataSnapshot.exists()) {
                         val nickname = dataSnapshot.child("nickname").getValue(String::class.java)
-                        if (nickname != null) {
-                            // 사용자의 닉네임을 가져왔으므로 이를 활용할 수 있습니다.
-                            mentiName = nickname // 멘티의 이름 변수에 저장
+                        nickname?.let {
+                            mentiName = it
                             Log.d("UserDisplayName", "User nickname: $mentiName")
-                            // 이후의 작업 코드 작성
-                            // 예: 데이터베이스에 값을 저장하는 등의 작업
-                        } else {
+                        } ?: run {
                             Log.e("UserDisplayName", "User nickname is null")
                         }
                     } else {
@@ -61,25 +57,28 @@ class MentiReserve : AppCompatActivity() {
                     Log.e("UserDisplayName", "Failed to read user info: ${databaseError.message}")
                 }
             })
-        } else {
+        } ?: run {
             Log.e("UserDisplayName", "Current user is null")
         }
-
-        // 현재 사용자의 uid 잘 가져오는지 검토하는 코드
-        currentUser?.let { Log.d("UserDisplayName", "Current user display UID: ${it.uid}") }
 
         val calendarView: MaterialCalendarView = findViewById(R.id.calendar_view)
         val timePicker: TimePicker = findViewById(R.id.timePicker)
         val reserveButton: Button = findViewById(R.id.reserve_button)
-
-        // 뒤로가기 버튼 활성화 (6/4(화))
         val arrowButton: ImageView = findViewById(R.id.arrow_3)
+
         arrowButton.setOnClickListener {
             onBackPressed()
         }
 
         coach_receiverUid = intent.getStringExtra("coach_uid").toString()
         coach_receiverName = intent.getStringExtra("coach_name").toString()
+
+        // Intent에서 "change" 플래그 확인
+        val isChange = intent.getBooleanExtra("change", false)
+        Log.d("change", "change : $isChange") // 로그 추가
+
+        val reserveId = intent.getStringExtra("reserve_id")
+        Log.d("MentiReserve", "Reserve ID: $reserveId") // 로그 추가
 
         reserveButton.setOnClickListener {
             val selectedDate = calendarView.selectedDate?.date ?: Calendar.getInstance().time
@@ -95,46 +94,50 @@ class MentiReserve : AppCompatActivity() {
                 timePicker.currentMinute
             }
             val time = String.format("%02d:%02d", hour, minute)
-            val datetime = "$day $time" // 날짜와 시간을 합친 문자열 생성
+            val datetime = "$day $time"
             Toast.makeText(this, "선택한 날짜: $day\n선택한 시간: $time", Toast.LENGTH_LONG).show()
 
-            // Firebase에 코치 정보 추가
             currentUser?.let { user ->
                 val mentiUid = user.uid
                 val dataMap = HashMap<String, Any>()
                 dataMap["menti_uid"] = mentiUid
-                dataMap["menti_name"] = mentiName // 멘티의 이름 데이터 추가
+                dataMap["menti_name"] = mentiName
                 dataMap["coach_receiverUid"] = coach_receiverUid
                 dataMap["coach_receiverName"] = coach_receiverName
                 dataMap["reserve_time"] = datetime
 
-                // Firebase에서 식별자 생성
-                // 중복을 피하기 위해 push() 메서드를 사용하여 새로운 노드 추가
-                val reserveInfoRef = database.getReference("reserveInfo").push()
-                val reserveId = reserveInfoRef.key
-
-                // 생성된 식별자를 사용하여 예약 정보 추가
-                if (reserveId != null) {
-                    reserveInfoRef.setValue(dataMap).addOnSuccessListener {
-                        // 데이터 전송이 성공한 경우
-                        // 추가 작업이 필요하다면 여기에 작성하세요
-                        val intent = Intent(this, Menti_scheduleActivity::class.java)
-                        intent.putExtra("reserveData", dataMap) // dataMap을 reserveData라는 이름으로 전달
-                        startActivity(intent)
-                    }.addOnFailureListener {
-                        // 데이터 전송이 실패한 경우
-                        // 실패 처리 작업을 여기에 작성하세요
-                        Toast.makeText(this, "Failed to save data to Firebase", Toast.LENGTH_SHORT).show()
-                    }
+                if (isChange && reserveId != null) {
+                    // 예약 정보 업데이트
+                    dataMap["reserveId"] = reserveId // reserveId를 추가
+                    reserveInfoRef.child(reserveId).updateChildren(dataMap)
+                        .addOnSuccessListener {
+                            Toast.makeText(this, "예약이 성공적으로 변경되었습니다.", Toast.LENGTH_SHORT).show()
+                            val intent = Intent(this, Menti_scheduleActivity::class.java)
+                            startActivity(intent)
+                        }
+                        .addOnFailureListener {
+                            Toast.makeText(this, "Firebase에 데이터를 업데이트하는 데 실패했습니다.", Toast.LENGTH_SHORT).show()
+                        }
                 } else {
-                    // 식별자 생성에 실패한 경우
-                    Log.e("MentiReserve", "Failed to generate reservation ID")
-                    Toast.makeText(this, "Failed to generate reservation ID", Toast.LENGTH_SHORT).show()
+                    // 새로운 예약 추가
+                    val newReserveId = reserveInfoRef.push().key
+                    if (newReserveId != null) {
+                        dataMap["reserveId"] = newReserveId // reserveId를 추가
+                        reserveInfoRef.child(newReserveId).setValue(dataMap)
+                            .addOnSuccessListener {
+                                Toast.makeText(this, "예약이 성공적으로 추가되었습니다.", Toast.LENGTH_SHORT).show()
+                                val intent = Intent(this, Menti_scheduleActivity::class.java)
+                                startActivity(intent)
+                            }
+                            .addOnFailureListener {
+                                Toast.makeText(this, "Firebase에 데이터를 저장하는 데 실패했습니다.", Toast.LENGTH_SHORT).show()
+                            }
+                    } else {
+                        Log.e("MentiReserve", "Failed to generate reservation ID")
+                        Toast.makeText(this, "예약 ID를 생성하는 데 실패했습니다.", Toast.LENGTH_SHORT).show()
+                    }
                 }
             }
         }
-
-
     }
-
 }
--- app/src/main/java/com/odal/wooco/Menti_scheduleActivityAdapter.kt
+++ app/src/main/java/com/odal/wooco/Menti_scheduleActivityAdapter.kt
@@ -1,7 +1,7 @@
 package com.odal.wooco
 
 import android.content.Context
-import android.content.Intent // Intent 클래스를 import 해주세요.
+import android.content.Intent
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -17,15 +17,10 @@ import com.odal.wooco.datamodels.ReserveDataModel
 
 class Menti_sheduleActivityAdapter(private var itemList: List<ReserveDataModel>, val context: Context) : RecyclerView.Adapter<Menti_sheduleActivityAdapter.ItemViewHolder>() {
 
-    data class Item(
-        val name: String,
-        val date: String
-    )
-
     class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val nameTextView: TextView = itemView.findViewById(R.id.menti_name2)
         val dateTextView: TextView = itemView.findViewById(R.id.class_day)
-        val changeClassButton: View = itemView.findViewById(R.id.change_class) // 버튼을 참조하는 View 추가
+        val changeClassButton: View = itemView.findViewById(R.id.change_class)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
@@ -38,9 +33,7 @@ class Menti_sheduleActivityAdapter(private var itemList: List<ReserveDataModel>,
         holder.nameTextView.text = item.coach_receiverName
         holder.dateTextView.text = item.reserve_time
 
-        // 아이템의 버튼에 OnClickListener 설정
         holder.changeClassButton.setOnClickListener {
-            // 코치 이름을 기반으로 코치 ID를 가져오기 위한 쿼리
             val coachName = item.coach_receiverName
             val coachInfoRef = FirebaseDatabase.getInstance().getReference("coachInfo")
                 .orderByChild("name")
@@ -49,24 +42,24 @@ class Menti_sheduleActivityAdapter(private var itemList: List<ReserveDataModel>,
             coachInfoRef.addListenerForSingleValueEvent(object : ValueEventListener {
                 override fun onDataChange(dataSnapshot: DataSnapshot) {
                     if (dataSnapshot.exists()) {
-                        // 쿼리 결과로부터 코치 ID 가져오기
                         val coachId = dataSnapshot.children.firstOrNull()?.key
+                        val reserveId = item.reserveId
 
-                        // 예약 화면으로 이동
-                        val intent = Intent(context, MentiReserve::class.java).apply{
-                            putExtra("change", true) // "change"라는 키워드를 인텐트에 추가
-                            putExtra("coach_uid", coachId) // 코치 ID를 전달
-                            putExtra("coach_name", coachName) // 코치 이름을 전달
+                        Log.d("Menti_sheduleAdapter", "reserve_id to send: $reserveId")
+
+                        val intent = Intent(context, MentiReserve::class.java).apply {
+                            putExtra("change", true)
+                            putExtra("coach_uid", coachId)
+                            putExtra("coach_name", coachName)
+                            putExtra("reserve_id", reserveId)
                         }
                         context.startActivity(intent)
                     } else {
-                        // 해당하는 코치 정보가 없는 경우 처리
                         Toast.makeText(context, "해당하는 코치를 찾을 수 없습니다.", Toast.LENGTH_SHORT).show()
                     }
                 }
 
                 override fun onCancelled(databaseError: DatabaseError) {
-                    // 데이터베이스 오류 처리
                     Log.e("Menti_sheduleAdapter", "Failed to read coach data: ${databaseError.message}")
                     Toast.makeText(context, "코치 정보를 가져오는 데 실패했습니다.", Toast.LENGTH_SHORT).show()
                 }
@@ -74,11 +67,8 @@ class Menti_sheduleActivityAdapter(private var itemList: List<ReserveDataModel>,
         }
     }
 
-
-
     override fun getItemCount(): Int = itemList.size
 
-    // 수업 시작 시간에 따라 itemList를 정렬하는 메서드
     fun sortByStartTime() {
         itemList = itemList.sortedBy { it.reserve_time }
         notifyDataSetChanged()
