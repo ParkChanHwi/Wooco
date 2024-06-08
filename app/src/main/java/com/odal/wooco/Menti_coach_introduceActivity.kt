9435db7046cc8cea9d34889496008d103215609f
ParkChanHwi <56109340+ParkChanHwi@users.noreply.github.com>
2024-06-08 14:22:09 +0900

    카테고리도 불러오는데 아직 아이템 추가x

--- app/src/main/java/com/odal/wooco/Menti_coach_introduceActivity.kt
+++ app/src/main/java/com/odal/wooco/Menti_coach_introduceActivity.kt
@@ -5,6 +5,10 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.firestore.FirebaseFirestore
 import com.odal.wooco.datamodels.CoachCategoryDataModel
 
@@ -24,22 +28,21 @@ class Menti_coach_introduceActivity : AppCompatActivity() {
     }
 
     private fun getCoachCategoryInfoFromDatabase(uid: String): List<CoachCategoryDataModel> {
-        // Firestore 데이터베이스에 접근하기 위한 Firestore 인스턴스 생성
-        val db = FirebaseFirestore.getInstance()
+        // Firebase Realtime Database에 접근하기 위한 DatabaseReference 인스턴스 생성
+        val categoriesRef = FirebaseDatabase.getInstance().getReference("coachCategoryRef").child(uid).child("category")
 
         // 카테고리 정보를 저장할 리스트 생성
         val coachCategoryInfoList = mutableListOf<CoachCategoryDataModel>()
 
-        // Firestore에서 해당 UID의 카테고리 정보를 가져오는 쿼리 실행
-        db.collection("category")
-            .whereEqualTo("coachUid", uid)
-            .get()
-            .addOnSuccessListener { documents ->
-                // 가져온 문서를 순회하면서 카테고리 정보를 추출하여 리스트에 추가
-                for (document in documents) {
-                    
-                    val category = document.getString("category") ?: ""
-                    val detail = document.getString("detail") ?: ""
+        // Firebase Realtime Database에서 해당 UID의 카테고리 정보를 가져오는 쿼리 실행
+        categoriesRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                // 가져온 스냅샷을 순회하면서 카테고리 정보를 추출하여 리스트에 추가
+                for (categorySnapshot in snapshot.children) {
+                    val category = categorySnapshot.child("category").getValue(String::class.java) ?: ""
+                    Log.d(TAG, "Category: $category")
+                    val detail = categorySnapshot.child("detail").getValue(String::class.java) ?: ""
+                    Log.d(TAG, "detail: $detail")
                     val coachCategoryDataModel = CoachCategoryDataModel(uid, category, detail)
                     coachCategoryInfoList.add(coachCategoryDataModel)
                 }
@@ -47,14 +50,17 @@ class Menti_coach_introduceActivity : AppCompatActivity() {
                 // RecyclerView의 어댑터에 데이터 변경을 알리고 업데이트
                 updateUI(coachCategoryInfoList)
             }
-            .addOnFailureListener { exception ->
+
+            override fun onCancelled(error: DatabaseError) {
                 // 실패 시 에러 처리
-                Log.e(TAG, "Error getting documents: ", exception)
+                Log.e(TAG, "Error getting documents: ", error.toException())
             }
+        })
 
         // 카테고리 정보 리스트 반환
         return coachCategoryInfoList
     }
+
 
 
     private fun updateUI(coachCategoryInfo: List<CoachCategoryDataModel>) {
