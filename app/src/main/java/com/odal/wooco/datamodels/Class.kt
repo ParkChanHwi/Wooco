b76b106304b7b25a5f19a99f6176de05928d5462
Parkchanhwi <cksgnl0523@gmail.com>
2024-06-09 16:42:29 +0900

    수정 전

---
+++ app/src/main/java/com/odal/wooco/datamodels/Class.kt
@@ -0,0 +1,11 @@
+package com.odal.wooco.datamodels
+
+data class Class(
+    val reserveId: String? = null,
+val mentiName: String = "",
+val coachName: String = "",
+val coachUid: String = "",
+val mentiUid: String = "",
+val mainID: String = "",
+val lastMessage: String = ""
+)
