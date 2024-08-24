caf982e6a9282546e7f8c5e8e443055f2282d8de
ParkChanHwi <56109340+ParkChanHwi@users.noreply.github.com>
2024-08-24 16:22:58 +0900

    지난수강 내역 뒤로가기

--- app/src/main/java/com/odal/wooco/Menti_mypage_pastclassActivity.kt
+++ app/src/main/java/com/odal/wooco/Menti_mypage_pastclassActivity.kt
@@ -1,7 +1,9 @@
 package com.odal.wooco
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -33,6 +35,12 @@ class Menti_mypage_pastclassActivity : AppCompatActivity() {
         } else {
             Toast.makeText(this, "User ID is missing", Toast.LENGTH_SHORT).show()
         }
+
+        val ArrowImageView: ImageView = findViewById(R.id.ArrowImageView)
+        ArrowImageView.setOnClickListener {
+            finish()
+        }
+
     }
 
     private fun fetchPastClassData(userId: String) {
